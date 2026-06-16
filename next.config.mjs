0eacf578788348caefcf2f88f6a7ea2ab3c71667
lapostekeryan <taooofff@gmail.com>
2026-06-16 11:21:33 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-};
-export default nextConfig;
